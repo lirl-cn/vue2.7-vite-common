ecdea0e22db319976a754f4a2975d5fe4fd73059
lirlu <this.lirlu@gmail.com>
2022-11-15 11:35:38 +0800

    Update defaultConfig.ts
    修改默认配置

--- config/defaultConfig.ts
+++ config/defaultConfig.ts
@@ -4,9 +4,9 @@ const config:DEFAULT_CONFIG_PROPS = {
   navBackgroundColor: '#000', // 左侧菜单背景颜色
   navTextColor: '#fff', // 左侧菜单文字颜色
   navActiveTextColor: '#ffd04b',  // 左侧菜单选中颜色
-  proxyBaseUrl: '/open/the/portal/api',  // 项目接口前缀（转发前缀标识）
-  proxyTargetUrl: 'http://10.16.22.38:36005/',  // 接口转发地址
-  tokenName: 'JTK', // token key
+  proxyBaseUrl: '/api',  // 项目接口前缀（转发前缀标识）
+  proxyTargetUrl: 'http://10.11.12.13:2333/',  // 接口转发地址
+  tokenName: 'token', // token key
   needFetchOtherMenuData: true,
 }
 export default config
